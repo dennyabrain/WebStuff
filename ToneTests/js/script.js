c501604a1f081df2724a78a01aa1b0ead46d1e25
dennyabrain <denny.george90@gmail.com>
2016-02-24 18:12:28 -0500

    nexus UI and bgcolor change running

--- ToneTests/js/script.js
+++ ToneTests/js/script.js
@@ -1,14 +1,34 @@
-/*var player = new Tone.Player("audio/soooha.mp3").toMaster();
-Tone.Buffer.on('load',function(){
-	player.start();
-})*/
+var feedback = 0.2;
+//var bgColor = ["aqua","yellow","black","red","blue","green"];
+var bgColor = ["#C53638","#F2E8D4","#2F2718","#55442D","#C72A25"];
+
+$(document).ready(function(){
+	//console.log($(document).width());
+	//console.log($(document).height());
+	$("#content").width($(document).width());
+	$("#content").height($(document).height());
+});
+
+nx.onload=function(){
+	dial1.on("*",function(data){
+		//console.log(data);
+	});
+
+	dial2.on("*",function(data){
+		feedback=data;
+		pingpong.feedback.value=data.value;
+		//console.log(data);
+	});
+};
+
 var notes=["C2","G2","D3","B2","C2","G2","D3","B2","C2","G2","D3","B2","C2","G2","B2","B2"]
 var index=0;
-var vol = new Tone.Volume(-12);
+var vol = new Tone.Volume(-24);
 var synth = new Tone.SimpleSynth({
 	"portamento": 0,
     "oscillator": {
-        "type": "square4"
+        "type": "square4",
+        "partials":[3,0,2,0,0.2]
     },
     "envelope": {
         "attack": 2,
@@ -16,11 +36,12 @@ var synth = new Tone.SimpleSynth({
         "sustain": 0.2,
         "release": 2
     }
-}).connect(vol);
+});
 var synth2 = new Tone.SimpleSynth({
 	"portamento": 0,
     "oscillator": {
-        "type": "square4"
+        "type": "square4",
+   		"partials":[4,0,2,0,0.2]
     },
     "envelope": {
         "attack": 2,
@@ -28,12 +49,13 @@ var synth2 = new Tone.SimpleSynth({
         "sustain": 0.2,
         "release": 2
     }
-}).connect(vol);
+});
 
 var synth3 = new Tone.SimpleSynth({
 	"portamento": 0,
     "oscillator": {
-        "type": "square4"
+        "type": "square4",
+    	"partials":[2,0,2,0,0.2]
     },
     "envelope": {
         "attack": 2,
@@ -41,26 +63,29 @@ var synth3 = new Tone.SimpleSynth({
         "sustain": 0.2,
         "release": 2
     }
-}).connect(vol);
-
-vol.toMaster();
-
-//synth.oscillator.partials=[1, 0, 2, 2, 2, 5, 6, 7];
-//synth.triggerAttack("C2");
+});
 
 var loop = new Tone.Loop(function(time){
-	
 	//var index=Math.floor(Math.random()*14);
 	index=index%16;
-	console.log(notes[index]);
-	var release = Math.random()*4;
+	//console.log(notes[index]);
+	//var release = Math.random()*4;
+	var release = dial1.val.value;
 	synth.triggerAttackRelease(notes[index],release);
 	synth2.triggerAttackRelease(notes[(index+3)%16],release+2);
-	synth2.triggerAttackRelease(notes[(index+2)%16],release+1);
+	synth3.triggerAttackRelease(notes[(index+2)%16],release+1);
 	index+=1;
+	console.log(Math.floor(Math.random()*6));
+	$("#content").css({
+		"background-color":bgColor[Math.floor(Math.random()*6)]
+	});
 },"1n").start();
 
+var pingpong = new Tone.PingPongDelay("4n",feedback);
 
+synth.chain(pingpong,vol,Tone.Master);
+synth2.chain(pingpong,vol,Tone.Master);
+synth3.chain(pingpong,vol,Tone.Master);
 
 Tone.Transport.bpm.value=60;
 loop.humanize="16n";
