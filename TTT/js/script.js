c5a85e5cae4548c308fc7e540cf69a88fc3e2926
dennyabrain <denny.george90@gmail.com>
2016-03-25 10:45:40 -0400

    added multiline support to TTT

--- TTT/js/script.js
+++ TTT/js/script.js
@@ -53,16 +53,43 @@ function greet(){
 	textSize(40);
 
 	var test=tale.split(' ');
+
+	/*if(test[0][5].indexOf('\n')!=-1){
+		console.log('contains newline');
+	}*/
+
 	var blockWidth=50;
 	offset=(360-numOfLines*40)/2;
 	var yIndex=130+offset;
 	var chars=0;
-	console.log(test);
+	//console.log(test);
 	for(i=0;i<test.length;i++){	
 		//console.log("hi");
-		chars+=test[i].length;
-		if(chars<25){
+		if(test[i].indexOf('\n')==-1){
+			chars+=test[i].length;
+			console.log('length of text is : '+chars);
+		}else{
+			chars+=test[i].indexOf('\n')-2;
+			console.log('length of newline text is : '+chars);
+		}
+		
+		if(chars<30){
+			var tempText //to save a token with \n after split
 			//console.log("hi2");
+			//check if it has newline
+			if(test[i].indexOf('\n')!=-1){
+				console.log('text i is : '+test[i]);
+				console.log('contains newline');
+				tempText=test[i].split('\n');
+				console.log('broken text is : '+tempText);
+				text(tempText[0],blockWidth,yIndex);
+				blockWidth=50;
+				yIndex+=40;
+				chars=0;
+				text(tempText[1],blockWidth,yIndex);
+				blockWidth+=textWidth(tempText[1]+' ');
+				continue;
+			}
 			text(test[i],blockWidth,yIndex);
 			blockWidth+=textWidth(test[i]+' ');
 		}else{
@@ -95,11 +122,18 @@ function calcNumOfLines(text){
 
 	//console.log(token);
 	var token=text.split(' ');
-	for(i=0;i<=token.length;i++){
+	//console.log('tokens : '+token);
+	
+	for(i=0;i<token.length;i++){
 		if(lineWidth+textWidth(token[i]+' ')<taleWidth){
 			fill(255,0,0);
 			//ellipse(50+lineWidth,130+numOfLines*40,10,10);
 			lineWidth+=textWidth(token[i]+' ');
+			//console.log('token number i : '+token[i]);
+			if(token[i].indexOf('\n')!=-1){
+				//console.log('contains newline');
+				numOfLines+=1;
+			}
 		}else{
 			numOfLines+=1;
 			//console.log("word before enter: "+token[i]);
